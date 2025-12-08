b3da46e2b287d399f1eb7d5c00a10129671f5c50
Ketelocele <135216378+Ketelocele@users.noreply.github.com>
2025-12-08 12:58:15 +0200

    Update link.php

--- link.php
+++ link.php
@@ -1,15 +1,5 @@
 <?php
-// link.php
 session_start();
-
-/**
- * If user clicks "Get Started" from index.php, use this file as a small router.
- * - If logged in -> dashboard.php
- * - If not logged in -> registration.php
- *
- * Also provides require_login() helper for protecting pages.
- */
-
 if (isset($_GET['start'])) {
     if (!empty($_SESSION['user_id'])) {
         header("Location: dashboard.php");
@@ -19,10 +9,9 @@ if (isset($_GET['start'])) {
     exit;
 }
 
-// Protect pages: call require_login() at top of protected pages
 function require_login() {
     if (empty($_SESSION['user_id'])) {
-        // Provide optional redirect param so login can send user back
+        
         header("Location: login.php?redirect=" . urlencode($_SERVER['REQUEST_URI']));
         exit;
     }
