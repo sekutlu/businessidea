6f676967c833921153b18a3c405f54036c912584
tlhoniLekhapelo <137868800+tlhoniLekhapelo@users.noreply.github.com>
2025-12-08 15:04:18 +0200

    Fix logout redirection path in logout.php

--- .idea/logout.php
+++ .idea/logout.php
@@ -16,6 +16,6 @@ header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: 0");
 
-header("Location: ../index.php?logout=success");
+header("Location: index.php?logout=success");
 exit();
 ?>
