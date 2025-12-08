5f17da2f40261f971d1d993bfd875fd90da2569c
Ketelocele <135216378+Ketelocele@users.noreply.github.com>
2025-12-08 12:47:03 +0200

    Update business idea.php

--- business idea.php
+++ business idea.php
@@ -1,12 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root";   // your MySQL username
-$password = "";       // your MySQL password (empty on XAMPP)
-$dbname = "business idea";     // CHANGE to your database name
+$username = "root";   
+$password = "";       
+$dbname = "business idea"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
